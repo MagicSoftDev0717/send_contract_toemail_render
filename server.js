8ac3021d587f77c451a9d4385802da43ae2ea7a4
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-07-22 23:37:38 +0700

    Update server.js #222

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/get-contract-file', async (req, res) => {
     return res.status(404).send('Contract not found');
   }
 
-  const { fileName } = contract; // Now we directly get the fileName for this contract
+   const { fileName, artistName, artistStreet, artistState, artistCountry, artistZip, artistEmail } = contract;
 
   const downloadsFolder = path.join(__dirname, 'contracts');  // Get the Downloads directory
   const filePath = path.join(downloadsFolder, fileName);  // Use Downloads folder for storing
