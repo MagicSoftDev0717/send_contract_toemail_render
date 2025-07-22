5ba212b1d77068e862e4a9cbb11894ee1ac06e21
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-07-23 01:23:37 +0700

    Update server.js #55555

--- server.js
+++ server.js
@@ -52,8 +52,7 @@ app.post('/send-contract-to-label', async (req, res) => {
     fs.writeFileSync(filePath, buffer);
 
     // Store the contract URL in the contract database
-    contractDatabase[contractId] = { fileName, artistName, artistStreet, artistState, artistCountry, artistZip, artistEmail,
-                                      labelName, labelStreet, labelState, labelCountry, labelZip, labelEmail };
+    contractDatabase[contractId] = { fileName, artistName, artistStreet, artistState, artistCountry, artistZip, artistEmail, labelName, labelStreet, labelState, labelCountry, labelZip, labelEmail };
 
     const msg = {
       to: labelEmail,
